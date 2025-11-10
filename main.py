6596e077e6a2ddd35ccd45918153179a9a7a3fdf
Marc-Diaz <marc.diaz.7e8@itb.cat>
2025-11-10 18:59:15 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,45 @@
 
+x = 2
+y = 2
+ejercicio1 = True
 def on_forever():
-    pass
+    global x
+    global y
+    global ejercicio1
+
+    if ejercicio1:
+        led.plot_bar_graph(input.temperature(), 50)
+    else:
+        led.plot(x, y)
+        basic.pause(50)
+        led.unplot(x, y)
+
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+
+
+        if accX <= 150 and x > 0:
+            x = x - 1
+        if accX > 150 and x < 4:
+            x = x + 1
+        if accY <= 150 and y > 0:
+            y = y - 1
+        if accY > 150 and y < 4:
+            y = y + 1
 basic.forever(on_forever)
+
+
+def on_button_pressed_a():
+    global ejercicio1
+    if ejercicio1 == True:
+        ejercicio1 = False
+    else:
+        ejercicio1 = True
+    basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            """)
+input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,3 +1,52 @@
+let x = 2
+let y = 2
+let ejercicio1 = true
 basic.forever(function on_forever() {
+    let accX: number;
+    let accY: number;
     
+    
+    
+    if (ejercicio1) {
+        led.plotBarGraph(input.temperature(), 50)
+    } else {
+        led.plot(x, y)
+        basic.pause(50)
+        led.unplot(x, y)
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+        if (accX <= 150 && x > 0) {
+            x = x - 1
+        }
+        
+        if (accX > 150 && x < 4) {
+            x = x + 1
+        }
+        
+        if (accY <= 150 && y > 0) {
+            y = y - 1
+        }
+        
+        if (accY > 150 && y < 4) {
+            y = y + 1
+        }
+        
+    }
+    
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    if (ejercicio1 == true) {
+        ejercicio1 = false
+    } else {
+        ejercicio1 = true
+    }
+    
+    basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
 })
